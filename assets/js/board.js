6f3042e16f191e3bcf526efc671cfb7a4a439874
Irving <moixllik@outlook.com>
2025-10-03 13:32:02 -0500

    dist

---
+++ assets/js/board.js
@@ -0,0 +1,157 @@
+'use strict';
+
+const sunColor = '#FFF';
+const moonColor = '#000';
+const boardColor = 'peru';
+const lineColor = '#222';
+const squareColor = '#0003';
+const hoverColor = '#0009';
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+// Config Canvas
+var box = 0;
+var mX = 0;
+var mY = 0;
+var boardImage = false;
+var sunImages = [];
+var moonImages = [];
+
+// Config Board
+var boardSquares = {};
+var parentState = 0;
+var sunState = 0;
+var moonState = 0;
+
+
+function draw() {
+    box = parseInt(canvas.offsetWidth / 7);
+
+    canvas.style.opacity = '0';
+    drawParent(sunImages, sunColor);
+    drawParent(moonImages, moonColor);
+    drawBoard();
+    canvas.style.opacity = '1';
+
+    drawSquares();
+}
+
+// Parent
+function drawParent(parent, color) {
+    const pos = [
+        [0.5, 0.2], // top
+        [0.2, 0.5], // left
+        [0.5, 0.8], // bottom
+        [0.8, 0.5], // right
+    ];
+    canvas.width = box;
+    canvas.height = box;
+
+    for (var p of pos) {
+        ctx.beginPath();
+        ctx.arc(box / 2, box / 2, box / 2, 0, Math.PI * 2);
+        ctx.arc(box * p[0], box * p[1], box / 6, 0, Math.PI * 2);
+        ctx.fillStyle = color;
+        ctx.fill('evenodd');
+
+        const img = new Image();
+        img.src = canvas.toDataURL();
+        parent.push(img);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+// Board
+function drawBoard() {
+    const b = box;
+
+    canvas.width = b * 7;
+    canvas.height = b * 7;
+
+    ctx.fillStyle = lineColor;
+    for (var x = 1; x < 7; x++) {
+        ctx.fillRect(x * b, 0, 0.01 * b, 7 * b);
+        ctx.fillRect(0, x * b, 7 * b, 0.01 * b);
+    }
+
+    ctx.fillStyle = squareColor;
+    // Islas
+    ctx.fillRect(0, 0, b, b);
+    ctx.fillRect(0, 6 * b, b, b);
+    ctx.fillRect(6 * b, 0, b, b);
+    ctx.fillRect(6 * b, 6 * b, b, b);
+    // Continente
+    ctx.fillRect(0, 3 * b, 7 * b, b);
+    ctx.fillRect(3 * b, 0, b, 7 * b);
+    ctx.fillRect(1 * b, 2 * b, 5 * b, 3 * b);
+    ctx.fillRect(2 * b, 1 * b, 3 * b, 5 * b);
+
+    boardImage = new Image();
+    boardImage.src = canvas.toDataURL();
+}
+
+function boardClear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(boardImage, 0, 0, canvas.width, canvas.height);
+}
+
+function addEventHover() {
+    canvas.addEventListener('mousemove', (event) => {
+        const rect = canvas.getBoundingClientRect();
+        mX = Math.floor((event.clientX - rect.left) / box);
+        mY = Math.floor((event.clientY - rect.top) / box);
+
+        boardClear();
+        ctx.fillStyle = hoverColor;
+        ctx.fillRect(mX * box, mY * box, box, box);
+
+        drawSquares();
+    });
+}
+
+// Squares
+function drawSquares() {
+    for (const key in boardSquares) {
+        const value = boardSquares[key];
+        const x = parseInt(key[1]) * box;
+        const y = parseInt(key[2]) * box;
+        const pad = box * 0.1;
+        const parent = value[0] == '+' ? 1 : -1;
+        const state = parseInt(value[1]);
+        const images = parent == 1 ? sunImages : moonImages;
+
+        ctx.drawImage(images[state], x + pad, y + pad, box * 0.8, box * 0.8);
+    }
+}
+
+// Text
+function drawText(x, y, size, color, text) {
+    ctx.font = parseInt(size * box) + 'px Arial';
+    ctx.fillStyle = color;
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText(text, x * box, y * box);
+}
+
+function drawRect(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * box, y * box, width * box, height * box);
+}
+
+function drawRectStroke(x, y, line, width, height, color) {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = line * box;
+    ctx.strokeRect(x * box, y * box, width * box, height * box);
+}
+
+function drawCircle(x, y, radio, color) {
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.arc(x * box, y * box, radio * box / 2, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+// Init
+window.addEventListener('resize', draw);
+draw();
---
+++ tutorial/main.js
@@ -0,0 +1,250 @@
+'use strict';
+
+const mainMenu = document.getElementById('main-menu');
+const barTop = document.getElementById('bar-top');
+const barBottom = document.getElementById('bar-bottom');
+
+// Config
+var mode;
+var pos;
+
+const tutorial = {
+    'board/0': () => {
+        const fontSize = 0.7;
+        const color = sunColor;
+
+        barBottom.innerHTML = '<b>Sun Player</b><br>Marks: GFEDCBA<br>Scales: 0123456';
+        drawText(3, 3.15, 0.8, color, '🌞');
+
+        const marks = 'GFEDCBA';
+
+        for (var i = 0; i < 7; i++) {
+            drawText(6.6, 6 - i, fontSize, color, i.toString());
+            drawText(i + 0.1, 6.4, fontSize, color, marks[i]);
+        }
+    },
+    'board/1': () => {
+        const fontSize = 0.7;
+        const color = moonColor;
+
+        barBottom.innerHTML = '<b>Moon Player</b><br>Marks: ABCDEFG<br>Scales: 0123456';
+        drawText(3, 3.15, 0.8, color, '🌜');
+
+        const marks = 'ABCDEFG';
+
+        for (var i = 0; i < 7; i++) {
+            drawText(0, 6.4 - i, fontSize, color, (6 - i).toString());
+            drawText(i + 0.5, 0, fontSize, color, marks[i]);
+        }
+    },
+    'board/2': () => {
+        barBottom.innerHTML = '<b>Yupana</b><br>Weights: 1235321<br>Scales: 0123456';
+
+        const marks = '1235321';
+        for (var i = 0; i < 7; i++) {
+            drawText(6.8, 6.1 - i, 0.3, sunColor, i.toString());
+            drawText(i + 0.05, 0.75, 0.2, moonColor, marks[i]);
+        }
+
+        // one
+        drawCircle(0.5, 0.5, 0.4, moonColor);
+        drawCircle(6.5, 0.5, 0.4, moonColor);
+
+        drawCircle(0.5, 6.5, 0.4, moonColor);
+        drawCircle(6.5, 6.5, 0.4, moonColor);
+
+        // two
+        drawCircle(5.5, 6.3, 0.3, moonColor);
+        drawCircle(5.5, 6.7, 0.3, moonColor);
+
+        drawCircle(1.5, 6.3, 0.3, moonColor);
+        drawCircle(1.5, 6.7, 0.3, moonColor);
+
+        // three
+        drawCircle(4.3, 6.3, 0.3, moonColor);
+        drawCircle(4.3, 6.7, 0.3, moonColor);
+        drawCircle(4.7, 6.5, 0.3, moonColor);
+
+        drawCircle(2.3, 6.3, 0.3, moonColor);
+        drawCircle(2.3, 6.7, 0.3, moonColor);
+        drawCircle(2.7, 6.5, 0.3, moonColor);
+
+        // five
+        drawCircle(3.4, 6.2, 0.2, moonColor);
+        drawCircle(3.4, 6.5, 0.2, moonColor);
+        drawCircle(3.4, 6.8, 0.2, moonColor);
+
+        drawCircle(3.6, 6.35, 0.2, moonColor);
+        drawCircle(3.6, 6.65, 0.2, moonColor);
+    },
+    'pieces/0': () => {
+        barBottom.innerHTML = '<b>Person</b><br>Move a position to any location.';
+
+        drawRect(2, 2, 3, 3, hoverColor);
+
+        ctx.drawImage(sunImages[0], 3.1 * box, 3.1 * box, box * 0.8, box * 0.8);
+    },
+    'pieces/1': () => {
+        barBottom.innerHTML = '<b>Military</b><br>Moves two positions horizontally or vertically.';
+
+        drawRect(1, 3, 5, 1, hoverColor);
+        drawRect(3, 1, 1, 5, hoverColor);
+
+        ctx.drawImage(sunImages[2], 3.1 * box, 3.1 * box, box * 0.8, box * 0.8);
+    },
+    'pieces/2': () => {
+        barBottom.innerHTML = '<b>Ruler</b><br>Moves two positions diagonally.<br>Right and left mean the same thing.';
+
+        drawRect(1, 1, 1, 1, hoverColor);
+        drawRect(2, 2, 1, 1, hoverColor);
+        drawRect(5, 1, 1, 1, hoverColor);
+        drawRect(4, 2, 1, 1, hoverColor);
+
+        drawRect(4, 4, 1, 1, hoverColor);
+        drawRect(5, 5, 1, 1, hoverColor);
+        drawRect(2, 4, 1, 1, hoverColor);
+        drawRect(1, 5, 1, 1, hoverColor);
+
+        ctx.drawImage(sunImages[1], 3.1 * box, 3.1 * box, box * 0.8, box * 0.8);
+
+        ctx.drawImage(moonImages[1], 0.1 * box, 0.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(moonImages[3], 6.1 * box, 0.1 * box, box * 0.8, box * 0.8);
+    },
+    'pieces/3': () => {
+        barBottom.innerHTML = '<b>Attack</b><br>Sun Player "attacks" Moon Player and replaces his position.';
+
+        drawRectStroke(3.025, 2.025, 0.05, 0.95, 4.95, hoverColor);
+        drawRectStroke(1.025, 4.025, 0.05, 4.95, 0.95, hoverColor);
+
+        ctx.drawImage(moonImages[0], 3.1 * box, 2.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(sunImages[2], 3.1 * box, 4.1 * box, box * 0.8, box * 0.8);
+    },
+    'pieces/4': () => {
+        barBottom.innerHTML = '<b>K.O.</b><br>The middle piece is removed from the board.';
+
+        drawRectStroke(0.025, 1.025, 0.05, 2.95, 0.95, hoverColor);
+
+        ctx.drawImage(moonImages[0], 0.1 * box, 1.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(sunImages[2], 1.1 * box, 1.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(moonImages[2], 2.1 * box, 1.1 * box, box * 0.8, box * 0.8);
+
+        drawRectStroke(5.025, 4.025, 0.05, 0.95, 2.95, hoverColor);
+
+        ctx.drawImage(sunImages[3], 5.1 * box, 4.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(moonImages[3], 5.1 * box, 5.1 * box, box * 0.8, box * 0.8);
+        ctx.drawImage(sunImages[1], 5.1 * box, 6.1 * box, box * 0.8, box * 0.8);
+    },
+    'squares/0': () => {
+        barBottom.innerHTML = '<b>Border</b><br>Squares in the border of the board.';
+
+        drawRect(0, 0, 7, 1, hoverColor);
+        drawRect(0, 1, 1, 5, hoverColor);
+        drawRect(6, 1, 1, 5, hoverColor);
+        drawRect(0, 6, 7, 1, hoverColor);
+    },
+    'squares/1': () => {
+        barBottom.innerHTML = '<b>Middle</b><br>Squares in the middle of the board.';
+
+        drawRect(1, 1, 5, 2, hoverColor);
+        drawRect(1, 3, 2, 1, hoverColor);
+        drawRect(4, 3, 2, 1, hoverColor);
+        drawRect(1, 4, 5, 2, hoverColor);
+
+        drawRect(3, 3, 1, 1, boardColor);
+    },
+    'squares/2': () => {
+        barBottom.innerHTML = '<b>Center & Peak</b><br>Square in the center of the board, also known as the mountain peak.';
+
+        drawRect(3, 3, 1, 1, '#0FFA');
+    },
+    'squares/3': () => {
+        barBottom.innerHTML = '<b>Mountain</b><br>';
+
+        drawRect(2, 2, 3, 1, '#F0FA');
+        drawRect(2, 3, 1, 1, '#F0FA');
+        drawRect(4, 3, 1, 1, '#F0FA');
+        drawRect(2, 4, 3, 1, '#F0FA');
+    },
+    'squares/4': () => {
+        barBottom.innerHTML = '<b>Jungle</b><br>';
+
+        drawRect(2, 1, 3, 1, '#0F0A');
+        drawRect(1, 2, 1, 3, '#0F0A');
+        drawRect(5, 2, 1, 3, '#0F0A');
+        drawRect(2, 5, 3, 1, '#0F0A');
+    },
+    'squares/5': () => {
+        barBottom.innerHTML = '<b>Coast</b><br>';
+
+        drawRect(3, 0, 1, 1, '#FF0A');
+        drawRect(0, 3, 1, 1, '#FF0A');
+        drawRect(6, 3, 1, 1, '#FF0A');
+        drawRect(3, 6, 1, 1, '#FF0A');
+    },
+    'squares/6': () => {
+        barBottom.innerHTML = '<b>Ocean</b><br>';
+
+        drawRect(1, 0, 2, 1, '#00FA');
+        drawRect(4, 0, 2, 1, '#00FA');
+
+        drawRect(0, 1, 2, 1, '#00FA');
+        drawRect(5, 1, 2, 1, '#00FA');
+
+        drawRect(0, 2, 1, 1, '#00FA');
+        drawRect(6, 2, 1, 1, '#00FA');
+
+        drawRect(0, 4, 1, 1, '#00FA');
+        drawRect(6, 4, 1, 1, '#00FA');
+
+        drawRect(0, 5, 2, 1, '#00FA');
+        drawRect(5, 5, 2, 1, '#00FA');
+
+        drawRect(1, 6, 2, 1, '#00FA');
+        drawRect(4, 6, 2, 1, '#00FA');
+    },
+    'squares/7': () => {
+        barBottom.innerHTML = '<b>Island</b><br>';
+
+        drawRect(0, 0, 1, 1, '#F00A');
+        drawRect(0, 6, 1, 1, '#F00A');
+        drawRect(6, 0, 1, 1, '#F00A');
+        drawRect(6, 6, 1, 1, '#F00A');
+    }
+};
+
+// General
+function load() {
+    const hash = location.hash.slice(1);
+    if (!hash) return;
+
+    const path = hash.split('/');
+    mode = path[0];
+    pos = parseInt(path[1]) || 0;
+
+    mainMenu.classList.add('none');
+    barTop.classList.remove('none');
+
+    barTop.querySelector('h4 span').innerHTML = mode.toUpperCase();
+    barBottom.innerHTML = '';
+
+    // Tutorial
+    const tuto = tutorial[hash];
+    if (tuto) {
+        if (boardImage.complete) boardClear();
+        tuto();
+    }
+}
+
+function toNext() {
+    const key = mode + '/' + (pos + 1);
+    location.hash = '#' + (tutorial.hasOwnProperty(key) ? key : (mode + '/0'));
+}
+
+function toPrev() {
+    const prev = pos - 1;
+
+    location.hash = '#' + mode + '/' + (prev < 0 ? 0 : prev);
+}
+
+window.addEventListener('hashchange', load);
+load();
